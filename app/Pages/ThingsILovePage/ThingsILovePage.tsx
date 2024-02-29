9cd9c029bd032c15f59a3cc7cc0de20fc77ee34e
Hind Guessous <guessoushind@gmail.com>
2024-02-29 14:49:56 +0100

    :sparkles: Added ThingsILovePage

---
+++ app/Pages/ThingsILovePage/ThingsILovePage.tsx
@@ -0,0 +1,11 @@
+import Title from "@/app/Components/Title"
+
+const containerStyle = { backgroundColor: "#38B6FF", height: "826px", display: "flex", alignItems: "center", justifyContent: "center" }
+
+export default function ThingsILovePage() {
+    return (
+        <section style={containerStyle}>
+            <Title title="Things I Love" color="#ffffff" />
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from 'next/font/google'
 import './reset-css.css'
 import BackgroundPage from "./Pages/BackgroundPage/BackgroundPage"
 import ShowcasePage from './Pages/ShowcasePage/ShowcasePage'
+import ThingsILovePage from './Pages/ThingsILovePage/ThingsILovePage'
 
 const open_sans = Open_Sans({
   subsets: ['latin']
@@ -14,6 +15,7 @@ export default function Home() {
       <HomePage />
       <BackgroundPage />
       <ShowcasePage />
+      <ThingsILovePage />
     </main>
   );
 }
