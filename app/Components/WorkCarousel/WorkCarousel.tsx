d45a00c901847c60048ecc870f547b5a24aa918b
Hind Guessous <guessoushind@gmail.com>
2024-03-05 17:14:50 +0100

    :sparkles: Add carousel slide

--- app/Components/WorkCarousel/WorkCarousel.tsx
+++ app/Components/WorkCarousel/WorkCarousel.tsx
@@ -1,11 +1,27 @@
+"use client"
 import CMA from './cma.png'
 import Sensia from './sensia.png'
 import LauraBeaute from './laura_beaute.png'
 import FestivalRecall from './festival_recall.png'
 import App from './app.png'
+import CarouselSlide from '../CarouselSlide/CarouselSlide'
+import data from '../../Data/work.json'
+import { useState } from 'react'
 
 export default function WorkCarousel() {
+    const carouselLength = data.projects.length
+    const [position, setPosition] = useState(1)
+    const [projectImage, setProjectImage] = useState(CMA)
+
     return (
-        <div></div>
+        <>
+            <CarouselSlide
+                name={data.projects[position].name}
+                description={data.projects[position].desc}
+                year={data.projects[position].project_year}
+                src={projectImage.src}
+                alt={data.projects[position].img_alt}
+            />
+        </>
     );
 }
